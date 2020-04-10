56ea0d64565d8110603d5cdb67c58e61d4f7df2e
CaoShangXi <1427026071qq@sina.com>
2020-04-10 23:19:51 +0800

    项目初始化

---
+++ ControllerExtensibility/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllerExtensibility.Models;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Result",new Result { ControllerName="Product",ActionName="Index"});
+        }
+
+        public ActionResult List()
+        {
+            return View("Result", new Result { ControllerName = "Product", ActionName = "List" });
+        }
+    }
+}
