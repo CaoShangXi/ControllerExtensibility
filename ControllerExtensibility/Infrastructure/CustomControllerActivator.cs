61014b25ee8cc40ac81556e06d3db282d97d96c7
CaoShangXi <1427026071qq@sina.com>
2020-04-12 23:25:00 +0800

    控制激活器

---
+++ ControllerExtensibility/Infrastructure/CustomControllerActivator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ControllerExtensibility.Controllers;
+
+namespace ControllerExtensibility.Infrastructure
+{
+    /// <summary>
+    /// 控制激活器
+    /// </summary>
+    public class CustomControllerActivator : IControllerActivator
+    {
+        public IController Create(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType==typeof(ProductController))
+            {
+                controllerType = typeof(CustomerController);
+            }
+            return (IController)DependencyResolver.Current.GetService(controllerType);//返回控制器实例
+        }
+    }
+}
