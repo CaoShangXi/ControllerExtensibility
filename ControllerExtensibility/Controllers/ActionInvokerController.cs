02aa2bc32f69e45e196eb1964b00e3de515f885c
CaoShangXi <1427026071qq@sina.com>
2020-04-14 23:48:39 +0800

    动作调用控制器，演示动作调用器的使用

---
+++ ControllerExtensibility/Controllers/ActionInvokerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllerExtensibility.Infrastructure;
+
+namespace ControllerExtensibility.Controllers
+{
+    /// <summary>
+    /// 动作调用控制器，演示动作调用器的使用
+    /// </summary>
+    public class ActionInvokerController : Controller
+    {
+        public ActionInvokerController()
+        {
+            this.ActionInvoker = new CustomActionInvoker();
+        }
+    }
+}
