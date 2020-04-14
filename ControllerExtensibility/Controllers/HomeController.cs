80025ff1f94f7c0d5d8c5a2900d43f7f41eb8a2a
CaoShangXi <1427026071qq@sina.com>
2020-04-15 00:29:26 +0800

    演示动作方法选择器的使用

---
+++ ControllerExtensibility/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllerExtensibility.Models;
+using ControllerExtensibility.Infrastructure;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View("Result", new Result { ControllerName = "Home", ActionName = "Index" });
+        }
+
+        [Local]
+        [ActionName("Index")]
+        public ActionResult LocalIndex()
+        {
+            return View("Result", new Result { ControllerName = "Home", ActionName = "LocalIndex" });
+        }
+    }
+}
