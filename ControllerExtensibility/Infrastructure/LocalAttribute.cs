3683f9faca99760c1fe948b4f950ffe0083c7cc5
CaoShangXi <1427026071qq@sina.com>
2020-04-15 00:28:43 +0800

    创建自定义动作方法选择器

---
+++ ControllerExtensibility/Infrastructure/LocalAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Infrastructure
+{
+    /// <summary>
+    /// 创建自定义动作方法选择器
+    /// </summary>
+    public class LocalAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return controllerContext.HttpContext.Request.IsLocal;
+        }
+    }
+}
