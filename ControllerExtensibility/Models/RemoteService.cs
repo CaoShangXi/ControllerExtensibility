e44b1e8c233b3bcfe549fdccc8ba0abff355d552
CaoShangXi <1427026071qq@sina.com>
2020-04-19 00:01:44 +0800

    异步方法

---
+++ ControllerExtensibility/Models/RemoteService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ControllerExtensibility.Models
+{
+    public class RemoteService
+    {
+        public string GetRemoteData()
+        {
+            Thread.Sleep(2000);
+            return "Hello from the other side of the world";
+        }
+
+        /// <summary>
+        /// 异步方法
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetRemoteDataAsync()
+        {
+            return await Task<string>.Factory.StartNew(()=> { Thread.Sleep(2000);return "Hello from the other side of the world"; });
+        }
+    }
+}
