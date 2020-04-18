f94c2754a5c7753eff766fa3dc83c8b61c2abc94
CaoShangXi <1427026071qq@sina.com>
2020-04-19 00:01:14 +0800

    异步控制器方法

---
+++ ControllerExtensibility/Controllers/RemoteDataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllerExtensibility.Models;
+using System.Threading.Tasks;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class RemoteDataController : Controller
+    {
+
+        //public ActionResult Data()
+        //{
+        //    RemoteService service=new RemoteService();
+        //    string data=service.GetRemoteData();
+        //    return View((object)data);
+        //}
+
+        public async Task<ActionResult> Data()
+        {
+            RemoteService service = new RemoteService();
+            string data=await Task<string>.Factory.StartNew(()=>  new RemoteService().GetRemoteData());
+            return View((object)data);
+        }
+
+        /// <summary>
+        /// 异步控制器方法
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> ConsumeAsyncMethod()
+        {
+            string data = await new RemoteService().GetRemoteDataAsync();
+            return View("Data",(object)data);
+        }
+    }
+}
